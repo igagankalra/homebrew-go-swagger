6d03f2ac62f278b41655cd716a1e30e742996708
Gagan Kalra <gagandeep.kalra@gartner.com>
2022-10-21 12:33:00 +0530

    adding isntall

--- go-swagger.rb
+++ go-swagger.rb
@@ -2,4 +2,24 @@ class GoSwagger < Formula
   on_macos do
     head "https://github.com/go-swagger/go-swagger.git" , branch: "master"
   end
+
+  option "with-goswagger", "Names the binary goswagger instead of swagger"
+
+  def install
+    nm = "swagger"
+    if build.with? "goswagger"
+      nm = "goswagger"
+    end
+    system "mv", @@filename, nm
+    bin.install nm
+  end
+
+  # test do
+  #   if build.with? "goswagger"
+  #     system "#{bin}/goswagger", "version"
+  #   else
+  #     system "#{bin}/swagger", "version"
+  #   end
+  # end
+
 end
